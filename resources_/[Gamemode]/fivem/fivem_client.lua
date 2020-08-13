6ad0faebbdac0aaea6abb16a021fdd3131be478b
WozzTv <63103641+WozzTv@users.noreply.github.com>
2020-08-13 23:12:01 +0200

    Add files via upload

---
+++ resources_/[Gamemode]/fivem/fivem_client.lua
@@ -0,0 +1,9 @@
+AddEventHandler('onClientMapStart', function()
+  exports.spawnmanager:setAutoSpawn(true)
+  exports.spawnmanager:forceRespawn()
+end)
+
+print("^0======================================================================^7")
+print("^0[^4Author^0] ^7 WozzTV^7")
+print("^0[^3Version^0] ^7:^0 ^01.0^7")
+print("^0======================================================================^7")
